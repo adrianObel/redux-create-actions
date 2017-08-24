bde20c0626080a58125af255e89bbae27d90e95a
Adrian Obelmejias <obeladrian@gmail.com>
2017-08-24 01:40:10 -0400

    Generate minified files with babel minify

--- rollup.config.js
+++ rollup.config.js
@@ -1,30 +1,45 @@
 import babel from 'rollup-plugin-babel'
+import minify from 'rollup-plugin-babel-minify'
 import pkg from './package.json'
 
-export default[
-  {
-    entry: 'src/index.js',
-    dest: pkg.browser,
-    format: 'umd',
-    moduleName: 'ReduxCreateAction',
-    plugins: [
-      babel()
-    ]
-  },
+const minifyName = file => file.replace(/\.js/, '.min.js')
+const plugins = minified => {
+  const plugins = [
+    babel()
+  ]
 
-  {
-    entry: 'src/index.js',
-    targets: [
-      {
-        dest: pkg.main,
-        format: 'cjs'
-      }, {
-        dest: pkg.module,
-        format: 'es'
-      }
-    ],
-    plugins: [
-      babel()
-    ]
+  if (minified) {
+    plugins.push(minify({ comments: false }))
   }
+
+  return plugins
+}
+
+const umd = ({ minified } = {}) => ({
+  entry: 'src/index.js',
+  dest: minified ? minifyName(pkg.browser) : pkg.browser,
+  format: 'umd',
+  moduleName: 'ReduxCreateAction',
+  plugins: plugins(minified)
+})
+
+const cjs = ({ minified } = {}) => ({
+  entry: 'src/index.js',
+  targets: [
+    {
+      dest: minified ? minifyName(pkg.main) : pkg.main,
+      format: 'cjs'
+    }, {
+      dest: minified ? minifyName(pkg.module) : pkg.module,
+      format: 'es'
+    }
+  ],
+  plugins: plugins(minified)
+})
+
+export default [
+  umd(),
+  cjs(),
+  umd({ minified: true }),
+  cjs({ minified: true })
 ]
